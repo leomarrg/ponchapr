3bfda45855c497d541d99bc12e14e5b1df9e8aa2
leomarrg <leomarrg@gmail.com>
2024-11-16 01:40:06 -0400

    fixed url

--- ponchapr_app/urls.py
+++ ponchapr_app/urls.py
@@ -14,7 +14,6 @@ urlpatterns = [
     path('checkout/', views.checkout_form, name='checkout_form'),
     path('terms/', views.terms_view, name='terms'),
     path('generate-report/', views.generate_report, name='generate_report'),
-    path('confirm-receipt/<str:unique_id>/', views.confirm_email_receipt, name='confirm_email_receipt'),
     # Add these new authentication paths
     path('accounts/login/', auth_views.LoginView.as_view(template_name='admin/login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
