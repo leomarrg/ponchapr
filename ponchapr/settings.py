b6d590f5fda8c43110754a1e80bdd04100b1a88f
leomar <leomarrg@gmail.com>
2025-03-20 00:50:13 -0400

    added mysql db

--- ponchapr/settings.py
+++ ponchapr/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-o--5o$jgl_+1l%3+5dw5gjj@z3v&k-%yl1m9a%wu^im+pwp^0b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.registratepr.com', 'registratepr.com']
 
 
 # Application definition
@@ -77,11 +77,25 @@ WSGI_APPLICATION = 'ponchapr.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'leomarrg$ponchapr_db',
+        'USER': 'leomarrg',
+        'PASSWORD': 'Italia101pr',
+        'HOST': 'leomarrg.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    },
 }
 
 
