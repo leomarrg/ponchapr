26e83872bc5c9f8b6e91ba2152d1ef08ac04872a
leomarrg <31631486+leomarrg@users.noreply.github.com>
2025-06-28 13:29:18 -0400

    testing new url

--- ponchapr/settings.py
+++ ponchapr/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 
 # Configuración de autenticación
 LOGIN_URL = '/admin/login/'
-LOGOUT_REDIRECT_URL = '/admin/login/'
+LOGOUT_REDIRECT_URL = 'admin/login/?next=/dashboard/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
