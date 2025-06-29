bf10d7aff51fb6f7fdad2833b3cdf2609d8f3767
leomarrg <31631486+leomarrg@users.noreply.github.com>
2025-06-29 15:59:50 -0400

    fixed settings file after adding presentacion and encuesta apps

--- ponchapr/settings.py
+++ ponchapr/settings.py
@@ -24,14 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o--5o$jgl_+1l%3+5dw5gjj@z3v&k-%yl1m9a%wu^im+pwp^0b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['www.registratepr.com', 'registratepr.com']
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost",
-    # add your real domain(s) here when you deploy
-]
+ALLOWED_HOSTS = ['www.registratepr.com', 'registratepr.com']
+# ALLOWED_HOSTS = [
+#     "127.0.0.1",
+#     "localhost",
+#     # add your real domain(s) here when you deploy
+# ]
 
 
 # Application definition
@@ -84,26 +84,26 @@ WSGI_APPLICATION = 'ponchapr.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'leomarrg$ponchapr_db',
-#         'USER': 'leomarrg',
-#         'PASSWORD': 'Italia101pr',
-#         'HOST': 'leomarrg.mysql.pythonanywhere-services.com',
-#         'PORT': '3306',
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-#         },
-#     },
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'leomarrg$ponchapr_db',
+        'USER': 'leomarrg',
+        'PASSWORD': 'Italia101pr',
+        'HOST': 'leomarrg.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    },
+}
 
 
 # Password validation
