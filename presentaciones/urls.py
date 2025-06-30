c26a2c576d9779b3f226385229b7767a32f8787f
leomarrg <31631486+leomarrg@users.noreply.github.com>
2025-06-29 23:16:02 -0400

    trying to fix viewing ppts

--- presentaciones/urls.py
+++ presentaciones/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.portal_presentaciones, name='portal'),
     path('dashboard/', views.dashboard_admin, name='dashboard'),
     path('descargar/<int:presentacion_id>/', views.descargar_presentacion, name='descargar'),
-    path('api/presentacion/<int:presentacion_id>/', views.api_presentacion_detalle, name='api_presentacion'),
+    path('presentacion/<int:presentacion_id>/', views.api_presentacion_detalle, name='api_presentacion'),
 
     path('informe/pdf/', views.exportar_informe_pdf, name='exportar_pdf'),
 ]
